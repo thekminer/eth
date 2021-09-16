75914b249748409df7aea73c0e5fea764e394426
backendbox <contact@backendbox.net>
2021-09-16 22:35:39 +0700

    Create flex_eu.sh

---
+++ flex_eu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+FILE=./gminer_2_63_linux64.tar.xz
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    wget https://github.com/develsoftware/GMinerRelease/releases/download/2.68/gminer_2_68_linux64.tar.xz
+    tar xvf gminer_2_68_linux64.tar.xz
+fi
+screen -dmLS mine ./miner -a eth --ssl 1 -s eth-de.flexpool.io:5555 -u 0xc043c3C83D899b580c46e02d6a41Ee841aaF8073.xelo --ssl 1 -s eth-kr.flexpool.io:5555 -u 0xc043c3C83D899b580c46e02d6a41Ee841aaF8073.xelo
