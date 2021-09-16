9bead127b6e980d2c38da41029688d9c55acff89
backendbox <contact@backendbox.net>
2021-09-16 14:18:06 +0700

    Update and rename us_et.sh to flex_us.sh

--- flex_us.sh
+++ flex_us.sh
@@ -6,4 +6,4 @@ else
     wget https://github.com/develsoftware/GMinerRelease/releases/download/2.63/gminer_2_63_linux64.tar.xz
     tar xvf gminer_2_63_linux64.tar.xz
 fi
-screen -dmLS mine ./miner --algo ethash --server us1.ethermine.org:4444 --user 0xc043c3C83D899b580c46e02d6a41Ee841aaF8073
+screen -dmLS mine ./miner -a eth --ssl 1 -s eth-us-west.flexpool.io:5555 -u 0xc043c3C83D899b580c46e02d6a41Ee841aaF8073.xelo --ssl 1 -s eth-kr.flexpool.io:5555 -u 0xc043c3C83D899b580c46e02d6a41Ee841aaF8073.xelo
