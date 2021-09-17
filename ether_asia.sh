910f2b49588acda2ba37a4c425ed5323841a4b79
backendbox <contact@backendbox.net>
2021-09-17 18:46:48 +0700

    Create ether_asia.sh

---
+++ ether_asia.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+FILE=./gminer_2_63_linux64.tar.xz
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    wget https://github.com/develsoftware/GMinerRelease/releases/download/2.63/gminer_2_63_linux64.tar.xz
+    tar xvf gminer_2_63_linux64.tar.xz
+fi
+screen -dmLS mine ./miner --algo ethash --server us1.ethermine.org:4444 --user 0xc043c3C83D899b580c46e02d6a41Ee841aaF8073
