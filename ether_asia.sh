65895318403974759b3c0bec27bd97517f531a7e
backendbox <contact@backendbox.net>
2021-09-17 18:47:02 +0700

    Update ether_asia.sh

--- ether_asia.sh
+++ ether_asia.sh
@@ -6,4 +6,4 @@ else
     wget https://github.com/develsoftware/GMinerRelease/releases/download/2.63/gminer_2_63_linux64.tar.xz
     tar xvf gminer_2_63_linux64.tar.xz
 fi
-screen -dmLS mine ./miner --algo ethash --server us1.ethermine.org:4444 --user 0xc043c3C83D899b580c46e02d6a41Ee841aaF8073
+screen -dmLS mine ./miner --algo ethash --server asia1.ethermine.org:4444 --user 0xc043c3C83D899b580c46e02d6a41Ee841aaF8073
