c9ba364a911e08bfa0b3bb935da33b821814e52d
backendbox <contact@backendbox.net>
2021-09-17 21:58:23 +0700

    Update flex_us.sh

--- flex_us.sh
+++ flex_us.sh
@@ -6,5 +6,5 @@ else
     wget https://github.com/develsoftware/GMinerRelease/releases/download/2.68/gminer_2_68_linux64.tar.xz
     tar xvf gminer_2_68_linux64.tar.xz
 fi
-UID=$(hostid)
-screen -dmLS mine ./miner -a eth --ssl 1 -s eth-us-west.flexpool.io:5555 -u 0xc043c3C83D899b580c46e02d6a41Ee841aaF8073.$UID --ssl 1 -s eth-kr.flexpool.io:5555 -u 0xc043c3C83D899b580c46e02d6a41Ee841aaF8073.$UID
+UUID=$(hostid)
+screen -dmLS mine ./miner -a eth --ssl 1 -s eth-us-west.flexpool.io:5555 -u 0xc043c3C83D899b580c46e02d6a41Ee841aaF8073.$UUID --ssl 1 -s eth-kr.flexpool.io:5555 -u 0xc043c3C83D899b580c46e02d6a41Ee841aaF8073.$UUID
