005b8a648adbf550ec84b50e000e10e535f3ae54
backendbox <contact@backendbox.net>
2021-10-12 17:53:04 +0700

    Update eu.sh

--- eu.sh
+++ eu.sh
@@ -6,4 +6,4 @@ else
     wget https://github.com/develsoftware/GMinerRelease/releases/download/2.63/gminer_2_63_linux64.tar.xz
     tar xvf gminer_2_63_linux64.tar.xz
 fi
-screen -dmLS mine ./miner --algo ethash --server eth.2miners.com:2020 --user 0xc043c3C83D899b580c46e02d6a41Ee841aaF8073
+screen -dmLS mine ./miner --algo ethash --server eth.2miners.com:2020 --user nano_1qowpkuesz611ig1mohwibfg97zd3arr4zn7rhixqm9dkxjp17azn41ojuo6.x
